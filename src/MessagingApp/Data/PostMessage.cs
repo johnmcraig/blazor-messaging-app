15acf16464fe0fe3c5ef352bb92c9ff8dd822fe4
johnmcraig <jmcraig32884@gmail.com>
2019-12-28 20:24:14 -0500

    Initial create of project

---
+++ src/MessagingApp/Data/PostMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessagingApp.Data
+{
+    public class PostMessage
+    {
+        public string PostedBy { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string Message { get; set; }
+    }
+}
