e003ed8ddedf9cff2d11e98483790700d629989d
johnmcraig <jmcraig32884@gmail.com>
2020-01-08 22:41:55 -0500

    Redifined wahte class would perform: read all and insert data

--- src/DataAccessLibrary/MessageData.cs
+++ src/DataAccessLibrary/MessageData.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using DataAccessLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataAccessLibrary
 {
     public class MessageData
     {
-        public string PostedBy { get; set; }
-        public DateTime PostedOn { get; set; }
-        public string Message { get; set; }
+        private readonly ISqlDataAccess _db;
+
+        public MessageData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<PostMessageModel>> GetMessages()
+        {
+            string sql = "select * from dbo.Message";
+
+            return _db.LoadData<PostMessageModel, dynamic>(sql, new { });
+        }
+
+        public Task InsertMessage(PostMessageModel message)
+        {
+            string sql = @"insert into dbo.Message () values ();";
+
+            return _db.SaveData(sql, message);
+        }
     }
 }
