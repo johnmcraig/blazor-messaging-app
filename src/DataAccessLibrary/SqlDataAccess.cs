8b9a666de3136257ad5073c6c50e5a60b9e2e665
johnmcraig <jmcraig32884@gmail.com>
2020-01-08 22:41:04 -0500

    Added interface to derive from

--- src/DataAccessLibrary/SqlDataAccess.cs
+++ src/DataAccessLibrary/SqlDataAccess.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLibrary
 {
-    public class SqlDataAccess
+    public class SqlDataAccess : ISqlDataAccess
     {
         private readonly IConfiguration _config;
 
